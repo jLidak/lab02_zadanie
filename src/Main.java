90e72a25a6384375cf3fc180b7a4ad95b6e7e581
stan10 <s97754@pollub.edu.pl>
2024-10-17 16:53:59 +0200

    feat: Logowanie i wylogowanie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        User user = new User("John", "Doe");
+        Login login = new Login(user);
+        login.login("John", "Doe");
+        System.out.println(user.isLoggedIn());
 
         Register register = new Register();
         register.registerUser();
